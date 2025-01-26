1386ceabcf8fb1f8b88884081739ffa90657d74a
HoneyVapor <honeyvapor.contact@gmail.com>
2025-01-26 15:34:03 -0600

    Update the API

--- api_data_control/views.py
+++ api_data_control/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.db import connection
 
 # Create your views here.
@@ -9,10 +9,12 @@ def ejemplo(request):
     return HttpResponse("Esto es un ejemplo de respuesta")
 
 def consulta_sql(request):
-    response = ""
+    data = []
     with connection.cursor() as cursor:
         cursor.execute("SELECT * FROM `users`")
+        columns = [col[0] for col in cursor.description] 
         rows = cursor.fetchall()
         for row in rows:
-            response += ' '.join(str(row))
-    return HttpResponse(response)
+            data.append(dict(zip(columns, row))) 
+
+    return JsonResponse(data, safe=False)
